42d7223af8ddf08edd16b010ec88df765bd80d62
unknown <divyacharaninabha@gmail.com>
2020-10-18 23:51:22 +0530

    Added: Functionality to read and write from json file added

--- src/main/java/com/bridgelabz/AddressBookIO/Main.java
+++ src/main/java/com/bridgelabz/AddressBookIO/Main.java
@@ -1,19 +1,22 @@
 package com.bridgelabz.AddressBookIO;
 
-
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvValidationException;
 
 public class Main {
 
-	public static final String path = "C:\\Users\\DELL\\eclipse-workspace\\AddressBookIO\\src\\main\\java\\com\\bridgelabz\\AddressBookIO\\addressbook-file.csv";
+	public static final String PATH_JSON = "C:\\Users\\DELL\\eclipse-workspace\\AddressBookIO\\src\\main\\java\\com\\bridgelabz\\AddressBookIO\\addressbook-json.json";
+	public static final String PATH_CSV = "C:\\Users\\DELL\\eclipse-workspace\\AddressBookIO\\src\\main\\java\\com\\bridgelabz\\AddressBookIO\\addressbook-file.csv";
 	List<Contact> contactList = new ArrayList<Contact>();
 
 	public static void main(String[] args) {
@@ -27,9 +30,35 @@ public class Main {
 		Main main = new Main();
 		Contact contact = main.getContact(sc);
 		contactList.add(contact);
-		main.writeData(contact);
-		main.readData();
+		main.writeDataJSON(contact);
+		main.readDataJSON();
 
+	}
+
+	// To read data from json file
+	private void readDataJSON() {
+
+		try {
+
+			Contact contact = new Gson().fromJson(new FileReader(PATH_JSON), Contact.class);
+			System.out.println(contact);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	// To write data into json file
+	private void writeDataJSON(Contact contact) {
+
+		try {
+			FileWriter writer = new FileWriter(PATH_JSON);
+			new Gson().toJson(contact, writer);
+			writer.close();
+		} catch (JsonIOException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	// To get contact from console
@@ -53,12 +82,12 @@ public class Main {
 		return (new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email));
 	}
 
-	// To write data into file
-	private void writeData(Contact contact) {
+	// To write data into csv file
+	private void writeDataCSV(Contact contact) {
 
 		CSVWriter writer = null;
 		try {
-			writer = new CSVWriter(new FileWriter(path));
+			writer = new CSVWriter(new FileWriter(PATH_CSV));
 			String[] header = { "First Name", "Last Name", "Address", "City", "State", "Zip", "Phone Number", "Email" };
 			writer.writeNext(header);
 			String[] info = contact.toString().split(" ");
@@ -69,18 +98,19 @@ public class Main {
 		}
 	}
 
-	// To read data into file
-	private void readData() {
+	// To read data from csv file
+	private void readDataCSV() {
 		CSVReader reader = null;
 		try {
-			reader = new CSVReader(new FileReader(path));
+			reader = new CSVReader(new FileReader(PATH_CSV));
 			String[] nextRecord;
 			while ((nextRecord = reader.readNext()) != null) {
-				for(String record : nextRecord) {
-					System.out.print(record+' ');
+				for (String record : nextRecord) {
+					System.out.print(record + ' ');
 				}
 				System.out.println();
 			}
+			reader.close();
 		} catch (IOException | CsvValidationException e) {
 			e.printStackTrace();
 		}
