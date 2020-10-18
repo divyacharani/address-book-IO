810881e9798ba7ad3707a801298297db0f205502
unknown <divyacharaninabha@gmail.com>
2020-10-18 16:26:39 +0530

    Added: Welcome message added

---
+++ src/main/java/com/bridgelabz/AddressBookIO/Main.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.AddressBookIO;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Welcome Message
+		System.out.println("Welcome to Address Book Program ");
+	}
+}
